c8eb4235ae22f8f0cade8605afacd5e97591e292
user <https://github.com/agent-37/java_calc.git>
2022-07-08 10:21:36 +0300

    class tree

--- src/main/java/com/company/Node.java
+++ src/main/java/com/company/Node.java
@@ -18,6 +18,13 @@ public class Node {
     public String getName() {
         return name;
     }
+    public void setName(String newName) {
+        name=newName;
+    }
+
+    public String getId() {
+        return id;
+    }
 
     public void add(Node t) {
         child.add(t);
@@ -26,17 +33,31 @@ public class Node {
     public List<Node> getChildren() {
         return child;
     }
-    public boolean findChildren(String _name){
-     boolean flag=false;
-     for(int i=0;i<child.size();i++){
-         if(_name.equals(child.get(i).getName())){
-             flag=true;
-         }
-     }
-     return flag;
+
+    public boolean findChildren(String _name) {
+        boolean flag = false;
+        for (int i = 0; i < child.size(); i++)
+            if (_name.equals(child.get(i).getName()))
+                flag = true;
+        return flag;
     }
+
     public void del_all_children() {
         child.clear();
+    }
+
+    public void del_child_id(String _id) {
+        for (int i = 0; i < child.size(); i++)
+            if (_id.equals(child.get(i).getId()))
+                child.remove(child.get(i));
+    }
+
+    public void change_child(String _id, String newName) {
+        for (int i = 0; i < child.size(); i++)
+            if (_id.equals(child.get(i).getId())){
+                child.get(i).setName(newName);
+                System.out.print("zdec");
+            }
     }
 
 }
--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -19,4 +19,51 @@ public class NodeTest {
         assertEquals(1,root.getChildren().size());
         assertEquals("Лист",root.getChildren().get(0).getName());
     }
+    @Test
+    void findChild(){
+        Node root=new Node("Корень");
+        Node child = new Node("Лист");
+        Node child1 = new Node("Лист1");
+        Node child2 = new Node("Лист2");
+        root.add(child);
+        root.add(child1);
+        root.add(child2);
+        assertEquals(true,root.findChildren("Лист"));
+    }
+    @Test
+    void delChild(){
+        Node root=new Node("Корень");
+        Node child = new Node("Лист");
+        Node child1 = new Node("Лист1");
+        Node child2 = new Node("Лист2");
+        root.add(child);
+        root.add(child1);
+        root.add(child2);
+        root.del_child_id(child1.getId());
+        assertEquals(false,root.findChildren("Лист1"));
+    }
+    @Test
+    void rezny(){
+        Node root=new Node("Корень");
+        Node child = new Node("Лист");
+        Node child1 = new Node("Лист1");
+        Node child2 = new Node("Лист2");
+        root.add(child);
+        root.add(child1);
+        root.add(child2);
+        root.del_all_children();
+        assertEquals(0,root.getChildren().size());
+    }
+    @Test
+    void renameChild(){
+        Node root=new Node("Корень");
+        Node child = new Node("Лист");
+        Node child1 = new Node("Лист1");
+        Node child2 = new Node("Лист2");
+        root.add(child);
+        root.add(child1);
+        root.add(child2);
+        root.change_child(child.getId(),"123");
+        assertEquals(true,root.findChildren(("123")));
+    }
 }
